550a2fe59aab568477147a2bbfa5ba6b545d7241
rafael.staine@aluno.ifsp.edu.br <rafael.staine@aluno.ifsp.edu.br>
2025-08-07 17:38:17 -0300

    Configurações iniciais

---
+++ src/pages/components/Topo/index.js
@@ -0,0 +1,18 @@
+export default function Topo() {
+
+    return (
+        <header>
+            <div>
+                <picture>
+                    <img src="./logo-sem-fundo.png" alt="Logo da empresa" />                    
+                </picture>
+
+                <nav>
+                    <a href="#">Produtos</a>
+                    <a href="#">Produtos</a>
+                    <a href="#">Produtos</a>
+                </nav>
+            </div>
+        </header>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
+import Topo from "./components/Topo";
 
 export default function Home() {
   return (
@@ -10,7 +11,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <div>
-       
+       <Topo />
       </div>
     </>
   );
